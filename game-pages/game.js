a964e584308fd5e3ad17bdff144ebe802ad2ccce
Infrayyy <99700988+Infrayyy@users.noreply.github.com>
2023-03-14 14:44:46 -0400

    Add files via upload

---
+++ game-pages/game.js
@@ -0,0 +1,11 @@
+function MK2() {
+    var win = window.open()
+    var url = "https://irunblocked.com/Emulators/SNES/Mortal-Kombat2/index.html"
+    var iframe = win.document.createElement('iframe')
+    iframe.style.width = "100%";
+    iframe.style.height = "100%";
+    iframe.style.border = "none";
+    iframe.src = url
+    win.document.body.appendChild(iframe)
+    win.document.title = "Google";
+}
